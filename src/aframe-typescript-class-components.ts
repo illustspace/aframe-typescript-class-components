15cfd6582ae2578be6fbd019d475c62396df671a
Will Ockelmann-Wagner <will@ae.studio>
2021-02-02 23:11:18 -0800

    move declared class into ts file

--- src/aframe-typescript-class-components.ts
+++ src/aframe-typescript-class-components.ts
@@ -1,12 +1,36 @@
 /* eslint-disable @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any */
 
-import { Schema, ComponentDefinition } from "aframe";
-import { AbstractBaseComponent } from "./AbstractBaseComponent";
+import { Schema, ComponentDefinition, Entity } from "aframe";
+import { Camera } from "three";
+
+/** A fake base Aframe component class that defines the methods Aframe will inject into the component at runtime. */
+declare class AbstractBaseComponent<D extends object = any> {
+  el: Entity;
+
+  schema: Schema<D>;
+  /** Re-define the default schema values and types here. */
+  data: D;
+  dependencies?: string[];
+  name: string;
+  events: Record<string, (event: any) => void>;
+
+  init(_data: D): void;
+  pause(): void;
+  play(): void;
+  remove(): void;
+  tick?(_time: number, _timeDelta: number): void;
+  tock?(_time: number, _timeDelta: number, _camera: Camera): void;
+  update(_oldData: D): void;
+  updateSchema?(): void;
+
+  extendSchema(_update: Schema): void;
+  flushToDOM(): void;
+}
 
 type ComponentMethods<T> = Record<keyof T, any> & { schema: Schema };
 type ComponentValues<T> = Record<keyof T, any>;
 
-/** A fake base Aframe component that defines the methods Aframe will inject into the component at runtime. */
+/** A class that describes the methods Aframe will inject into the component at runtime. */
 export const BaseComponent = class EmptyComponent {} as typeof AbstractBaseComponent;
 
 interface GenericBaseComponent<D extends object> {
